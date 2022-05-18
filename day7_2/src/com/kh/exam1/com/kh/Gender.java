c747de3d3126408ac51818fc849e5a9dee3c9c38
ggong00 <103275351+ggong00@users.noreply.github.com>
2022-05-18 18:25:00 +0900

    Delete day7_2 directory

--- day7_2/src/com/kh/exam1/com/kh/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.kh.exam1.com.kh;
-
-public enum Gender {
-    MEN, WOMEN
-}
--- day7_2/src/com/kh/exam1/com/kh/PersonMain2.java
+++
@@ -1,8 +0,0 @@
-package com.kh.exam1.com.kh;
-
-public class PersonMain2 {
-    public static void main(String[] args) {
-        Person p1 = new Person("홍길동");
-
-    }
-}
