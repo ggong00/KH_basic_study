4c14651a54ed5b0fd68f7f897f6c70e2ca44e325
ggong00 <103275351+ggong00@users.noreply.github.com>
2022-05-19 17:35:38 +0900

    Add files via upload

---
+++ day8_2/src/com/kh/exam5/Book.java
@@ -0,0 +1,17 @@
+package com.kh.exam5;
+
+public class Book {
+    private String title;
+
+    public Book() {
+        super();
+    }
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
