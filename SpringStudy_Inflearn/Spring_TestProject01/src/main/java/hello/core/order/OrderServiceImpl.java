24946084807c38451bb7d342d062e7c1b1c5048d
ggong00 <103275351+ggong00@users.noreply.github.com>
2022-06-26 01:11:31 +0900

    Add files via upload

---
+++ SpringStudy_Inflearn/Spring_TestProject01/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,26 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderServiceImpl implements OrderService {
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        int discount = discountPolicy.discount(memberRepository.findById(memberId),itemPrice);
+        Order order = new Order(memberId,itemName,itemPrice,discount);
+        return order;
+    }
+
+}
